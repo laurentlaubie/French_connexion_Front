298f2729f76f337974c842878d9406d2ddd58df9
Lucasmillet00 <lucamillet@gmail.com>
2021-05-04 10:38:50 +0200

    style: structure of mobile navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -4,8 +4,8 @@ import './navBar.scss';
 
 const NavBar = () => (
   <div className="navBar">
-    <ul>
-      <li className="navBar__Profile"> Mon Profil/se connecter </li>
+    <ul className="navBar__List">
+      <li className="navBar__Profile"> Mon Profil se_connecter </li>
       <li className="navBar__Network"> Notre Réseau </li>
       <li className="navBar__Search"> Rechercher </li>
     </ul>
