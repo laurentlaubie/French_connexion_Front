e06d224d64e59af3ce8ab0202f56248d3ba6ce7d
Lucasmillet00 <lucamillet@gmail.com>
2021-05-04 14:34:35 +0200

    feat: adding header on mobile and desktop

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 import './navBar.scss';
+import network from 'src/assets/pictures/network.png';
+import profile from 'src/assets/pictures/profile.png';
+import search from 'src/assets/pictures/search.png';
 
 const NavBar = () => (
+  
   <div className="navBar">
+    <div className="navBar__Rounded">.</div>
     <ul className="navBar__List">
-      <li className="navBar__Profile"> Mon Profil se_connecter </li>
-      <li className="navBar__Network"> Notre Réseau </li>
-      <li className="navBar__Search"> Rechercher </li>
+      <li className="navBar__Profile"> <img src={profile} /> <div className="navBar__Title"> Mon Profil  </div> </li>
+      <li className="navBar__Network"> <img src={network} /> <div className="navBar__Title"> Notre Réseau </div> </li>
+      <li className="navBar__Search"> <img src={search} /> <div className="navBar__Title"> Rechercher </div> </li>
     </ul>
   </div>
 );
