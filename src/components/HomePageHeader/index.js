06bd9c648326b0762f552db86fae1b8add7e5d29
Lagaalett <daval.clemence@gmail.com>
2021-05-05 14:30:34 +0200

    feature : modification of component HomePageHeader. Adding of style. Responsive still in progress

--- src/components/HomePageHeader/index.js
+++ src/components/HomePageHeader/index.js
@@ -9,15 +9,19 @@ import header1 from '../../assets/images/cherry-728.svg';
 import './homePageHeader.scss';
 
 const HomePageHeader = () => (
-  <div
-    className="homePageHeader"
-    style={{ backgroundImage: `url(${header1})` }}
-  >
-    <NavBar />
-    {/* <HomePageTitle /> */}
-    <SearchBar />
-
+  <div className="homePageHeader">
+    <div className="homePageHeader__image">
+      <img src={header1} alt="Un vieux monsieur qui va prendre l'avion" />
+    </div>
+    <div className="homePageHeader__content">
+      <h1 className="homePageHeader__content__title"> French connection</h1>
+      <h2 className="homePageHeader__content__subtitle"> La <span> communauté </span> qui vous aide à voyager</h2>
+    </div>
+    <div className="homePageHeader__searchbar">
+      <SearchBar />
+    </div>
   </div>
+
 );
 
 export default HomePageHeader;
