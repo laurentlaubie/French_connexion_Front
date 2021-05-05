dace189ad8d4d88d2a9230db5863a8cc6fea9dba
Lagaalett <daval.clemence@gmail.com>
2021-05-05 15:37:25 +0200

    feat: installation of redux dependencies, creation of a store and a reducer. creation of a connected component NavBar

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+  isConnected: false,
+};
+
+export default (state = initialState, action = {}) => {
+  switch (action.type) {
+    // case :
+    //   return {
+    //     ...state,
+    //   };
+    default:
+      return state;
+  }
+};
