3d06bdd3b4e38f813a41d6e4bade78afaa686139
Lagaalett <daval.clemence@gmail.com>
2021-05-05 21:13:47 +0200

    adding of content in signIn component + styling (whithout opening/closing fonctionnality)

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -4,7 +4,17 @@ import './signIn.scss';
 
 const SignIn = () => (
   <div className="signIn">
-    Je suis le componant signIn
+    <div className="signIn__modal">
+      <h1 className="signIn__modal__title"> Bienvenue sur French Connection </h1>
+      <button className="signIn__modal__closeButton" type="button"> X </button>
+      <form className="signIn__modal__form">
+        <input className="signIn__modal__form__field" type="email" name="email" placeholder="Email" />
+        <input className="signIn__modal__form__field" type="password" name="password" placeholder="Mot de passe" />
+        <button className="signIn__modal__form__button" type="submit"> Se connecter </button>
+      </form>
+
+      <a className="signIn__link" href="#" > Vous n'avez pas encore de compte ?</a>
+    </div>
   </div>
 );
 
