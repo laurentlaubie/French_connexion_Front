2fc4201525282d93df5ac152f8d156b4a8ca17e5
laurentlaubie <laurent.laubie31@gmail.com>
2021-05-07 09:24:52 +0200

    fix:resolving bug for profil__service

--- src/components/ModifyProfile/index.js
+++ src/components/ModifyProfile/index.js
@@ -15,26 +15,31 @@ const ModifyProfile = ({dataHobbies, dataServices }) => {
   <div className="modifyprofile">
     <h2 className="modifyprofile__title">Modifier mon profil </h2>
       <div className="modifyprofile__allinfos">
+
         <form className="modifyprofile__form">
-        <input className="modifyprofile__form__field" type="pseudo" name="pseudo" placeholder="Pseudo" />
-        <input className="modifyprofile__form__field" type="email" name="email" placeholder="Adresse Mail" />
-        <input className="modifyprofile__form__field" type="firstname" name="firstname" placeholder="Nom" />
-        <input className="modifyprofile__form__field" type="lastname" name="lastname" placeholder="Prénom" />
-        <input className="modifyprofile__form__field" type="city" name="city" placeholder="Ville" />
-        <input className="modifyprofile__form__field" type="Country" name="Country" placeholder="Pays" />
-        <input className="modifyprofile__form__field" type="Phone" name="CountPhonery" placeholder="Téléphone" />
+          <label for="Pseudo"> Pseudo</label>
+            <input className="modifyprofile__form__field" type="pseudo" name="pseudo" placeholder="Pseudo" />
+            <input className="modifyprofile__form__field" type="email" name="email" placeholder="Adresse Mail" />
+            <input className="modifyprofile__form__field" type="firstname" name="firstname" placeholder="Nom" />
+            <input className="modifyprofile__form__field" type="lastname" name="lastname" placeholder="Prénom" />
+            <input className="modifyprofile__form__field" type="city" name="city" placeholder="Ville" />
+            <input className="modifyprofile__form__field" type="Country" name="Country" placeholder="Pays" />
+            <input className="modifyprofile__form__field" type="Phone" name="CountPhonery" placeholder="Téléphone" />
         </form>
 
-        <button className="modifyprofile__mp" type="submit"> Modifier mon mot de passe </button>
-        
-        <div className="profile__interest">
-          <p>bieres</p>
-        </div>
+          <button className="modifyprofile__mp" type="submit"> Modifier mon mot de passe </button>    
+      </div>
+      <div className="profile__services">
+          <input type="checkbox" id="hobby" name="hobby" />
+          <label for="hobby">test</label>
+      </div>
+      
+          
         <div className="profile__services">
           <p>soirée de fou</p>
         </div>
-      </div>
   </div>
+  
   )
 };
 
