da1d258f1a33398d00f002f1d83d9fa215969ace
laurentlaubie <laurent.laubie31@gmail.com>
2021-05-18 11:44:57 +0200

    style-Loader--DONE

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -4,7 +4,23 @@ import React from 'react';
 import './loading.scss';
 
 const Loading = () => (
-  <div className="loading">Veuillez patienter</div>
+  
+  <div className="loader-wrapper">
+	<div className="loader">
+    <div className="roller"></div>
+    <div className="roller"></div>
+  </div>
+  
+  <div id="loader2" className="loader">
+    <div className="roller"></div>
+    <div className="roller"></div>
+  </div>
+  
+  <div id="loader3" className="loader">
+    <div className="roller"></div>
+    <div className="roller"></div>
+  </div>
+</div>
 );
 
 export default Loading;
