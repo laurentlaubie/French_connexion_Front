690e3b0b32887599256c9259bafc45689345d69f
Clemence <daval.clemence@gmail.com>
2021-05-21 12:11:06 +0200

    feat: addition of message when the datas are null in profile page + conditionnal vue for Map

--- src/components/ProfileLocalisation/index.js
+++ src/components/ProfileLocalisation/index.js
@@ -10,26 +10,48 @@ import './profileLocalisation.scss';
 import colombie from 'src/assets/images/colombie.png';
 //import MapProfile from '../MapProfile';
 
-const ProfileLocalisation = ({ name, longitude, latitude, country }) => {
-  let countryName = '';
-  if (country != null) {
-    countryName = country.name;
+const ProfileLocalisation = ({ name, longitude, latitude, country, isMyProfile, username }) => {
+  // let countryName = '';
+  // if (country != null) {
+  //   countryName = country.frenchName;
+  // }
+  // else {
+  //   countryName = '';
+  // }
+
+  let emptyMessage = '';
+  if (isMyProfile){
+    emptyMessage = 'Vous n\'avez pas indiqué votre ville de résidence.' ;
   }
-  else {
-    countryName = '';
+  else{
+    emptyMessage = `${username} n'a pas indiqué sa ville de résidence.`;
   }
 
   return (
     <div className="profileLocalisation">
-      <div className="profileLocalisation__title"> {name}, {countryName} </div>
-      <div className="profileLocalisation__map"> <MapProfile latProp={latitude} lngProp={longitude} /> </div>
+      <div className="profileLocalisation__title"> {name != '' ? `${name}, ${country.frenchName}` : '' }</div>
+      <div className="profileLocalisation__empty"> {name === '' ? emptyMessage : '' }</div>
+      <div className="profileLocalisation__map"> 
+        {name != '' && <MapProfile latProp={latitude} lngProp={longitude} />}
+      </div>
     </div>
   );
 };
-// ProfileLocalisation.propTypes = {
-//   cityId: PropTypes.number.isRequired,
-//   cityName: PropTypes.string.isRequired,
-//   countryName: PropTypes.string.isRequired,
-// };
+
+ProfileLocalisation.propTypes = {
+  name: PropTypes.string,
+  latitude: PropTypes.number,
+  longitude: PropTypes.number,
+  country: PropTypes.string,
+  isMyProfile: PropTypes.bool.isRequired,
+  username: PropTypes.string.isRequired,
+};
+
+ProfileLocalisation.defaultProps = {
+  name: '',
+  longitude: '',
+  latitude: '',
+  country: '',
+}
 
 export default ProfileLocalisation;
