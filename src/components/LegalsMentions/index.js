34dc8691d560964b92af42abfa67e8d0455a4af0
laurentlaubie <laurent.laubie31@gmail.com>
2021-05-21 12:30:36 +0200

    style-legalsmentions page--DOING

--- src/components/LegalsMentions/index.js
+++ src/components/LegalsMentions/index.js
@@ -6,31 +6,48 @@ import './legalsMentions.scss';
 const LegalsMentions = () => (
   <div className="legalsmention">
     <div className="legalsmention__title" > Mentions Legales</div>
-    <div className="legalsmention__title__subtitle" > Informations legales et conditions d'utilisation</div>
-    <div className="legalsmention__texts">
-      <p className="legalsmention__texts__bloc"> 
-      
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris cursus mattis molestie a iaculis at. Dui faucibus in ornare quam viverra orci sagittis. Malesuada pellentesque elit eget gravida cum sociis natoque penatibus et. Vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur. Tellus orci ac auctor augue mauris augue neque. Purus sit amet volutpat consequat. Eu mi bibendum neque egestas congue quisque egestas diam. Natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Fusce id velit ut tortor pretium. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra accumsan. Id leo in vitae turpis massa sed. Odio eu feugiat pretium nibh ipsum. Tincidunt dui ut ornare lectus sit amet est placerat. Eu ultrices vitae auctor eu augue ut. Arcu cursus euismod quis viverra nibh cras. Eget velit aliquet sagittis id consectetur purus ut. Nibh tellus molestie nunc non. Sapien et ligula ullamcorper malesuada proin libero. Velit laoreet id donec ultrices tincidunt arcu non sodales neque.
+      <div className="legalsmention__title__subtitle" > Informations legales et conditions d'utilisation</div>
+       <div className="legalsmention__texts">
+        <div className="legalsmention__texts__bloc"> 
+          <div className="legalsmention__texts__part">
+            Merci de lire avec attention les différentes modalités d’utilisation du présent site avant d’y parcourir ses pages. En vous connectant sur ce site, vous acceptez, sans réserves, les présentes modalités.
+            Aussi, conformément à l’article n°6 de la Loi n°2004-575 du 21 Juin 2004 pour la confiance dans l’économie numérique, les responsables du présent site internet www.anthedesign.fr sont :
+          </div>
+          <div className="legalsmention__texts__part">
+            Éditeur du Site :
+            SARL FrenchConnection Numéro de SIRET : 85321735600027
+            Responsable éditorial : Hugo ESSIQUE
+            12 Rue du Huit Mai 1945, 60350 ATTICHY
+            Téléphone : 09 00 00 00 00
+            Email : contact@FrenchConnection.fr
+            Site Web : www.FrenchConnection.fr
+          </div>
+          <div className="legalsmention__texts__part">
+            Hébergement :
 
-       Morbi enim nunc faucibus a pellentesque sit. Adipiscing enim eu turpis egestas pretium aenean pharetra. Porta lorem mollis aliquam ut porttitor leo a. Nibh praesent tristique magna sit amet purus gravida. Elit sed vulputate mi sit. Sit amet luctus venenatis lectus magna fringilla urna porttitor. Penatibus et magnis dis parturient montes nascetur. Vulputate ut pharetra sit amet aliquam id diam maecenas. Ut enim blandit volutpat maecenas. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam.
+            Hébergeur : SARL FrenchConnection
+            12 Rue du Huit Mai 1945, 60350 ATTICHY
+            Site Web : www.FrenchConnection.fr
 
-       Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet risus. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis. In iaculis nunc sed augue lacus viverra. Sit amet aliquam id diam maecenas ultricies mi. Nullam non nisi est sit amet facilisis magna etiam tempor. Suspendisse faucibus interdum posuere lorem. Turpis egestas sed tempus urna. Eu volutpat odio facilisis mauris sit amet massa. Quis lectus nulla at volutpat diam ut venenatis. Mi bibendum neque egestas congue quisque.
+            Développement :
 
-       Egestas sed sed risus pretium. Viverra maecenas accumsan lacus vel facilisis volutpat est. Erat imperdiet sed euismod nisi porta lorem mollis aliquam. Magnis dis parturient montes nascetur. Condimentum vitae sapien pellentesque habitant morbi tristique senectus et. Pellentesque id nibh tortor id aliquet lectus. Arcu risus quis varius quam quisque id. Sodales neque sodales ut etiam sit amet nisl purus in. Gravida quis blandit turpis cursus. Est sit amet facilisis magna etiam tempor orci eu lobortis. Tellus orci ac auctor augue mauris augue neque gravida. Tincidunt arcu non sodales neque sodales ut. Enim neque volutpat ac tincidunt vitae. Sed euismod nisi porta lorem mollis aliquam ut porttitor. Purus sit amet luctus venenatis lectus magna fringilla urna. Vitae congue mauris rhoncus aenean vel elit scelerisque mauris. Vel risus commodo viverra maecenas accumsan lacus vel.
+            SARL FrenchConnection
 
-       Aliquam nulla facilisi cras fermentum odio eu feugiat pretium nibh. Viverra vitae congue eu consequat. Ut faucibus pulvinar elementum integer enim. Blandit libero volutpat sed cras. Egestas sed tempus urna et pharetra pharetra massa massa ultricies. Non tellus orci ac auctor augue mauris augue neque. Risus sed vulputate odio ut. Facilisi nullam vehicula ipsum a arcu cursus. Faucibus interdum posuere lorem ipsum dolor sit. Ut enim blandit volutpat maecenas. Fusce ut placerat orci nulla pellentesque dignissim enim sit. Dictum non consectetur a erat nam at lectus. Faucibus pulvinar elementum integer enim neque volutpat. Habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim.
+            Adresse : 12 Rue du Huit Mai 1945, 60350 ATTICHY
 
-       Turpis nunc eget lorem dolor sed viverra ipsum nunc aliquet. Tincidunt eget nullam non nisi est. Vel pharetra vel turpis nunc eget lorem dolor sed. Viverra ipsum nunc aliquet bibendum enim facilisis. Nibh sit amet commodo nulla facilisi. Orci phasellus egestas tellus rutrum tellus pellentesque. Erat pellentesque adipiscing commodo elit. A erat nam at lectus urna duis convallis. Porttitor eget dolor morbi non. Tellus pellentesque eu tincidunt tortor. Nunc lobortis mattis aliquam faucibus purus in. Vel orci porta non pulvinar. Habitant morbi tristique senectus et netus et. Amet porttitor eget dolor morbi non arcu. Elit ut aliquam purus sit amet luctus venenatis lectus. Ipsum a arcu cursus vitae congue. Nisi lacus sed viverra tellus in hac habitasse platea. Arcu cursus euismod quis viverra nibh cras pulvinar. Vehicula ipsum a arcu cursus vitae congue.
-
-       Nunc faucibus a pellentesque sit. Viverra ipsum nunc aliquet bibendum enim. Pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Eu volutpat odio facilisis mauris sit. Scelerisque felis imperdiet proin fermentum leo vel orci porta non. Nibh mauris cursus mattis molestie a. Gravida rutrum quisque non tellus orci ac auctor. Adipiscing tristique risus nec feugiat in fermentum posuere urna. Quisque sagittis purus sit amet volutpat consequat. Vitae purus faucibus ornare suspendisse sed nisi. Platea dictumst quisque sagittis purus sit. Vulputate dignissim suspendisse in est. Mi eget mauris pharetra et ultrices neque ornare aenean. Eu scelerisque felis imperdiet proin fermentum leo vel. Diam sit amet nisl suscipit.
-
-      </p>
-
-      
-     
-    
-    </div>
-</div>
+            Site Web : www.FrenchConnection.fr
+            </div>
+            <div className="legalsmention__texts__part">
+            <div className="legalsmention__texts__title">Conditions d’utilisation :</div>
+            <a>Ce site (www.anthedesign.fr) est proposé en différents langages web (HTML, HTML5, Javascript, CSS, etc…) pour un meilleur confort d’utilisation et un graphisme plus agréable.
+            Nous vous recommandons de recourir à des navigateurs modernes comme Internet explorer, Safari, Firefox, Google Chrome, etc…
+            L’agence web AntheDesign met en œuvre tous les moyens dont elle dispose, pour assurer une information fiable et une mise à jour fiable de ses sites internet.
+            Toutefois, des erreurs ou omissions peuvent survenir. L’internaute devra donc s’assurer de l’exactitude des informations auprès de AntheDesign , et signaler toutes modifications du site qu’il jugerait utile. AntheDesign n’est en aucun cas responsable de l’utilisation faite de ces informations, et de tout préjudice direct ou indirect pouvant en découler.
+            </a>
+            </div>
+        </div>
+      </div>
+  </div>
 
 );
 
