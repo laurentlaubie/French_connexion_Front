6d5d6ccbdae4aeaa9f48025424b8be32714cbabd
laurentlaubie <laurent.laubie31@gmail.com>
2021-05-21 18:21:56 +0200

    style AboutUs2--DONE

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -9,13 +9,15 @@ import './aboutUs.scss';
 
 const AboutUs = ({dataTeam}) => (
 <div className="aboutUs">
-  <h1 className="aboutUs__title" > A Propos de nous</h1>
-  <h2 className="aboutUs__title__subtitle" > Le Projet French Connection</h2>
-  <div className="aboutUs__text">
-  <p> Tout commence un soir de confinement lors d'un apéro visio !</p>
-  <p>"On n'a qu'a partir en voyage en Colombie des que le confinement est fini ? " pose soudainement la question Nicolas.</p>
-  <p> Euh, franchement ça craint labas non ? et ya quoi a faire, on y connait personne en plus...nous repondions tous</p>
-  <p> De la, l'idée d'un site communautaire d'entraide entre voyageurs et residants est née <span className="site">- French Connection - </span></p>
+  <div className="aboutUs__title" > A Propos de nous</div>
+  <div className="aboutUs__global">
+    <h2 className="aboutUs__title__subtitle" > Le Projet French Connection</h2>
+    <div className="aboutUs__text">
+    <p> Tout commence un soir de confinement lors d'un apéro visio !</p>
+    <p>`On n'a qu'a partir en voyage en Colombie dès que le confinement est fini ? `</p>
+    <p> `Euh, franchement ça craint la-bas non ? et y'a quoi à faire? on y connait personne en plus...` nous repondions tous</p>
+    <p> De là, l'idée d'un site communautaire d'entraide entre voyageurs et residants est née <span className="site">- French Connection - </span></p>
+    </div>
   </div>
   <h2 className="aboutUs__title__subtitle">L'Equipe</h2>
   
