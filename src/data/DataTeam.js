540150ffdc648e013d40a838656b8f04a7c51da1
Clemence <daval.clemence@gmail.com>
2021-05-24 19:04:08 +0200

    feat: improvement of abous us page design

--- src/data/DataTeam.js
+++ src/data/DataTeam.js
@@ -1,42 +1,32 @@
 export default [
   {
     id: 1,
-    avatar: 'https://cdn.icon-icons.com/icons2/2643/PNG/512/male_boy_person_people_avatar_icon_159358.png',
+    avatar: 'clemence.jpg',
     name: 'Clémence',
-    role: 'Scrum Master',
-    description: 'Je vieng de Rouen. La Normandie tu l\'aimes ou tu la quittes',
-    
+    role: 'Lead-Dev Front',
   },
   {
     id: 2,
-    avatar: 'https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png',
+    avatar: 'lucas.png',
     name: 'Lucas',
     role: 'Référent Technique Git',
-    description: 'Je vieng de VillePreux. Paris tu l\'aimes ou tu la quittes',
-
   },
   {
-   id: 3,
-   avatar: 'https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png',
+    id: 3,
+    avatar: 'https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png',
     name: 'Thomas',
     role: 'Lead-Dev Back',
-    description: 'Je vieng du je sais pas. Je sais pas tu l\'aimes ou tu le quittes',
-
   },
   {
-    id: 4,avatar: 'https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png',
+    id: 4,
+    avatar: 'https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png',
     name: 'Laurent',
-    role: 'Lead-Dev Front',
-    description: 'Je viengde Toulouse. Le sud tu l\'aimes ou tu le quittes',
-
+    role: 'Scrum Master',
   },
   {
     id: 5,
     avatar: 'https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png',
     name: 'Tom',
     role: 'Product owner',
-    description: 'Je viens de Saint Quentin. Paris tu l\'aimes ou tu la quittes',
-
   },
-  
-]
+];
